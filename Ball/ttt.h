aa770c5b8b95c704f35549478c9ee527747122d6
nanwujue <1107894385@qq.com>
2016-07-05 14:40:22 +0800

    add ttt

---
+++ Ball/ttt.h
@@ -0,0 +1,13 @@
+//
+//  ttt.h
+//  Ball
+//
+//  Created by yangyunen on 16/7/5.
+//  Copyright © 2016年 yangyunen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ttt : NSObject
+
+@end
---
+++ Ball/ttt.m
@@ -0,0 +1,13 @@
+//
+//  ttt.m
+//  Ball
+//
+//  Created by yangyunen on 16/7/5.
+//  Copyright © 2016年 yangyunen. All rights reserved.
+//
+
+#import "ttt.h"
+
+@implementation ttt
+
+@end
